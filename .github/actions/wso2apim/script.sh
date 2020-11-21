73f8cdaefce5ec41254cec695d5084d9bd7ed826
mihinduranasinghe <mihindu.21523@gmail.com>
2020-11-21 17:14:27 +0530

    add environment wso2apicloud using apimcli automated

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -12,8 +12,19 @@ echo "::error ::Error Message"
 echo "::end-group"
 
 
-echo "::group::Some expandable logs"
+echo "::group::Add environment wso2apicloud"
 apimcli --help
+# apimcli remove env wso2apicloud
+
+apimcli add-env -n wso2apicloud \
+                      --registration https://gateway.api.cloud.wso2.com/client-registration/register \
+                      --apim https://gateway.api.cloud.wso2.com/pulisher \
+                      --token https://gateway.api.cloud.wso2.com/token \
+                      --import-export https://gateway.api.cloud.wso2.com/api-import-export \
+                      --admin https://gateway.api.cloud.wso2.com/api/am/admin/ \
+                      --api_list https://gateway.api.cloud.wso2.com/api/am/publisher/apis \
+                      --app_list https://gateway.api.cloud.wso2.com/api/am/store/applications
+          
 echo "::end-group"
 
 # echo "::set-env name=HELLO::hello"
