f79963a4c7ecd5f8ba6f2ec8b4226d5277194e07
mihinduranasinghe <mihindu.21523@gmail.com>
2020-11-21 18:57:30 +0530

    wso2apicloud CI automate dev to prod

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -1,10 +1,8 @@
 #!/bin/sh -l
 
-echo "hi"
-echo "Hello $1"
-echo "::add-mask::$1"
 echo "Hello $1"
 
+# echo "::add-mask::$1"
 # echo "::group::Some expandable warnings"
 # echo "::debug ::Debug Message"
 # echo "::warning ::Warning Message"
@@ -21,7 +19,6 @@ echo "::end-group"
 
 echo "::group::Add environment wso2apicloud"
 apimcli remove env wso2apicloud
-
 apimcli add-env -n wso2apicloud \
                       --registration https://gateway.api.cloud.wso2.com/client-registration/register \
                       --apim https://gateway.api.cloud.wso2.com/pulisher \
@@ -37,15 +34,15 @@ echo "::end-group"
 echo "::group::Export API from current tenant"
 # apimcli export-api -n <API-name> -v <version> -r <provider> -e <environment> -u <username> -p <password> -k
 # apimcli export-api --name <API-name> --version <version> --provider <provider> --environment <environment> --username <username> --password <password> --insecure
-apimcli login wso2apicloud -u mihindu@wso2.com@production -p $2 -k
-apimcli export-api -n TeamMasterAPI -v v1.0.0 -r mihindu@wso2.com@production -e wso2apicloud -k
+apimcli login wso2apicloud -u mihindu@wso2.com@development -p $2 -k
+apimcli export-api -n TeamMasterAPI -v v1.0.0 -r mihindu@wso2.com@development -e wso2apicloud -k
 apimcli logout wso2apicloud 
 echo "::end-group"
 
 echo "::group::Import API to targetted tenant"
 # apimcli import-api -f <environment>/<file> -e <environment> -u <username> -p <password> --preserve-provider <preserve_provider> -k
 # apimcli import-api --file <environment>/<file> --environment <environment> --username <username> --password <password> --preserve-provider <preserve_provider> --insecure
-apimcli login wso2apicloud -u mihindu@wso2.com@development -p $2 -k
+apimcli login wso2apicloud -u mihindu@wso2.com@production -p $2 -k
 apimcli import-api -f wso2apicloud/TeamMasterAPI_v1.0.0.zip -e wso2apicloud --preserve-provider=false -k
 apimcli logout wso2apicloud 
 echo "::end-group"
@@ -53,7 +50,7 @@ echo "::end-group"
 echo "::group::List APIS in a perticular environment"
 # apimcli list apis -e <environment> -k
 # apimcli list apis --environment <environment> --insecure
-apimcli list apis -e wso2apicloud -k
+# apimcli list apis -e wso2apicloud -k
 echo "::end-group"
 
 echo "::group::Export an *App from current tenant"
@@ -74,13 +71,24 @@ echo "::group::List *Apps in a perticular environment"
 # apimcli list apps -e wso2apicloud -o admin -k
 echo "::end-group"
 
-echo "::group::List *Reset Users"
+echo "::group:: Reset Users"
 # If you want to list APIs or applications that belong to a particular tenant, 
 # you need to first reset the user before listing APIs or applications for the particular tenant.
 
 # apimcli reset-user -e <environment>
 # apimcli reset-user --environment <environment>
 # apimcli reset-user -e wso2apicloud
+echo "::end-group"
+
+echo "::group:: Set HTTP request timeout "
+# apimcli set --http-request-timeout <http-request-timeout>
+# apimcli set --http-request-timeout 10000
+echo "::end-group"
+
+echo "::group:: Set export directory "
+#Run the following CLI command to the change the default location of the export directory.
+# apimcli set --export-directory <export-directory-path>
+# apimcli set --export-directory /Users/kim/Downloads/MyExports
 echo "::end-group"
 
 
