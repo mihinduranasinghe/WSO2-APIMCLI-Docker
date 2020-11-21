f2988b1d2e1af27c0c36771d5d34127c7cecffd8
mihinduranasinghe <mihindu.21523@gmail.com>
2020-11-21 20:07:15 +0530

    API project initalizatin in vm and clone into repo

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -33,10 +33,23 @@ echo "::end-group"
 
 echo "::group::Setup API in development tenant"
 # apictl import-api -f $API_DIR -e $DEV_ENV -k --preserve-provider --update --verbose
+pwd
+git init
+git remote add origin "https://$GITHUB_ACTOR:${{secrets.GITHUB_TOKEN}}@github.com/$GITHUB_REPOSITORY.git"
+git config --global user.email "my-bot@bot.com"
+git config --global user.name "my-bot"
+git fetch 
+git checkout main
+git branch --set-upstream-to=origin/main
+git pull 
+ls -a 
+# echo $RANDOM >> random.txt
 apimcli init SampleStore --oas petstore.json --definition api_template.yaml
-ls
-cd SampleStore
-ls
+ls -a 
+git add . 
+git commit -m "API project initialized"
+git push
+
 apimcli login wso2apicloud -u mihindu@wso2.com@development -p $2 -k
 apimcli import-api -f $GITHUB_WORKSPACE -e wso2apicloud --preserve-provider --update --verbose -k
 apimcli logout wso2apicloud 
