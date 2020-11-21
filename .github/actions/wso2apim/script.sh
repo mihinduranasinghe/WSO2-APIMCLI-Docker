9509514e6a425e10bab71f44b61095142b17acc0
mihinduranasinghe <mihindu.21523@gmail.com>
2020-11-21 16:05:36 +0530

    Grouping expandable echo

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -1,3 +1,21 @@
 #!/bin/sh -l
 
+
 echo "hi"
+
+
+echo "::debug ::Debug Message"
+echo "::warning ::Warning Message"
+echo "::error ::Error Message"
+
+echo "::add-mask::$1"
+echo "Hello $1"
+
+time=$(date)
+echo "::set-output name=time::$time"
+
+echo "::group::Some expandable logs"
+apimcli --help
+echo "::end-group"
+
+# echo "::set-env name=HELLO::hello"
